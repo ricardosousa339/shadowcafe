a11e01952ab9f5d08adf52a618218935a2b7ba21
Ricardo Henrique <ricardosousa339@gmail.com>
2024-10-30 18:27:30 -0300

    Add README instructions for executable generation and enhance start screen info display

---
+++ game/README.md
@@ -0,0 +1,2 @@
+comando pra gerar o executavel no windows
+pyinstaller --noconsole --onefile --add-data="game/src/assets/*:game/src/assets/" game/src/main.py
--- game/src/start_screen.py
+++ game/src/start_screen.py
@@ -1,10 +1,12 @@
 import pygame
+import time  # Import time module
 
 from settings import TITLE
 from credits import CreditsScreen  # Import the CreditsScreen class
 
 class StartScreen:
     def __init__(self, screen, width, height):
+        self.start = False
         self.screen = screen
         self.width = width
         self.height = height
@@ -33,7 +35,63 @@ class StartScreen:
             self.button_credits.get_width() + 2 * self.button_padding,
             self.button_credits.get_height() + 2 * self.button_padding
         )
+        self.blink_font = pygame.font.Font("game/src/assets/fonts/homevideo.ttf", 30)
+        self.blink_text = self.blink_font.render("Pressione enter para pular", True, (255, 255, 255))
+        self.blink_rect = self.blink_text.get_rect(center=(self.width // 2, self.height - 50))
         
+    def show_info(self):
+        info_font = pygame.font.Font("game/src/assets/fonts/homevideo.ttf", 23)
+        lines1 = [
+            "Bem vindo ao Light Café!",
+            "A escuridão avança e a sua única defesa é um cafezinho.",
+            "Não desperdice nenhum ingrediente,",
+            "ou seu destino será trevoso"
+        ]
+        lines2 = [
+            "Use as teclas laterais para se mover",
+            "e dar sabor ao seu café!",
+            "◀️     ▶️",
+            "Boa sorte!"
+        ]
+        self.screen.fill((6, 2, 37))
+        y_offset = 100
+        line_spacing = 30  # Add spacing between lines
+        for line in lines1:
+            info_text = info_font.render(line, True, (255, 255, 255))
+            self.screen.blit(info_text, (self.width // 2 - info_text.get_width() // 2, y_offset))
+            y_offset += info_font.get_height() + line_spacing 
+
+        y_offset += 150
+        for line in lines2:
+            info_text = info_font.render(line, True, (255, 255, 255))
+            self.screen.blit(info_text, (self.width // 2 - info_text.get_width() // 2 , y_offset))
+            y_offset += info_font.get_height() + line_spacing
+
+        pygame.display.flip()
+        start_time = time.time()
+        while time.time() - start_time < 8:  # Wait for 8 seconds
+            self.screen.fill((6, 2, 37))
+            y_offset = 100
+            for line in lines1:
+                info_text = info_font.render(line, True, (255, 255, 255))
+                self.screen.blit(info_text, (self.width // 2 - info_text.get_width() // 2, y_offset))
+                y_offset += info_font.get_height() + line_spacing 
+
+            y_offset += 150
+            for line in lines2:
+                info_text = info_font.render(line, True, (255, 255, 255))
+                self.screen.blit(info_text, (self.width // 2 - info_text.get_width() // 2 , y_offset))
+                y_offset += info_font.get_height() + line_spacing
+
+            # Blinking text
+            if int(time.time() * 2) % 2 == 0:  # Toggle every half second
+                self.screen.blit(self.blink_text, self.blink_rect)
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                    self.start = False
+                    return
+
     def show(self):
         self.screen.fill((6, 2, 37))
         
@@ -54,20 +112,22 @@ class StartScreen:
         
         pygame.display.flip()
 
-        start = True
-        while start:
+        self.start = True
+        while self.start:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    start = False
+                    self.start = False
                     return False 
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     if self.button_rect.collidepoint(event.pos):
-                        start = False
+                        self.start = False
+                        self.show_info()
                         return True 
                     elif self.button_credits_rect.collidepoint(event.pos):
                         credits_screen = CreditsScreen(self.screen, self.width, self.height)
                         credits_screen.run()
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN: 
-                        start = False
+                        self.start = False
+                        self.show_info()
                         return True
